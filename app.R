ee56fa5f97b5b3f16671e5cbd5ffdb0cf1693a9f
waseem <waseemmedhatg@gmail.com>
2020-05-17 19:12:53 +0200

    wrapped steps inwell panels

--- app.R
+++ app.R
@@ -11,43 +11,49 @@ ui <- fluidPage(
   actionLink('main_help', 'What is this about?', icon('question-circle')),
   fluidRow(br()),
   
-  h3('Step 1: load the data'),
-  fluidRow(
-    column(
-      5,
-      selectInput(
-        'builtin_dataset',
-        'Choose a built-in dataset:',
-        choices = set_choices,
-        selected = 'mtcars'
-      )
-    ),
-    column(2, strong('or...')),
-    column(5, fileInput('upload_dataset', 'Upload your own dataset:'))
-  ),
-  
-  h3('Step 2: choose a variable'),
-  fluidRow(
-    column(5, textInput('var', 'Enter variable name:')),
-    column(
-      7,
-      strong('Available (numeric) variables:'),
-      em(textOutput('var_names'))
+  wellPanel(
+    h3('Step 1: load the data'),
+    fluidRow(
+      column(
+        5,
+        selectInput(
+          'builtin_dataset',
+          'Choose a built-in dataset:',
+          choices = set_choices,
+          selected = 'mtcars'
+        )
+      ),
+      column(2, strong('or...')),
+      column(5, fileInput('upload_dataset', 'Upload your own dataset:'))
     )
   ),
   
-  h3('Tests'),
-  fluidRow(
-    column(4, h4('Histogram'), fluidRow(br()), plotOutput('hist')),
-    column(4, h4('Q-Q plot'), fluidRow(br()), plotOutput('qq')),
-    column(
-      4,
-      h4('Goodness-of-fit tests'),
-      fluidRow(br()),
-      tableOutput('gof'),
-      fluidRow(br()),
-      h4('Sample size'),
-      textOutput('n')
+  wellPanel(
+    h3('Step 2: choose a variable'),
+    fluidRow(
+      column(5, textInput('var', 'Enter variable name:')),
+      column(
+        7,
+        strong('Available (numeric) variables:'),
+        em(textOutput('var_names'))
+      )
+    )
+  ),
+  
+  wellPanel(
+    h3('Tests'),
+    fluidRow(
+      column(4, h4('Histogram'), fluidRow(br()), plotOutput('hist')),
+      column(4, h4('Q-Q plot'), fluidRow(br()), plotOutput('qq')),
+      column(
+        4,
+        h4('Goodness-of-fit tests'),
+        fluidRow(br()),
+        tableOutput('gof'),
+        fluidRow(br()),
+        h4('Sample size'),
+        textOutput('n')
+      )
     )
   )
   
